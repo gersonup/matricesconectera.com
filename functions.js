b781131aedccc15d65ac713cd5dd982fc5932005
Gerson Ricardo Navarro Hernandez <gersonup@gmail.com>
2023-01-19 13:55:14 -0500

    adding bootstrap

--- functions.js
+++ functions.js
@@ -1,7 +1,4 @@
-
-
 $(document).ready(function(){
-
     const characters = {
         "A": [
             [1, 1, 1, 1, 1],
@@ -348,7 +345,6 @@ $(document).ready(function(){
     
 
     function createWord(word_array) {
-
         for (let i = 0; i < word_array.length; i++) {
             $(".main").append(`<div class="character" id="${i}"></div>`);
             createCharacter(characters[word_array[i]], i);
@@ -359,13 +355,17 @@ $(document).ready(function(){
         for (let j = 0; j < character_matrix.length; j++) {
             for (let k = 0; k < character_matrix.length; k++) {
                 if (character_matrix[j][k] == 1){
-                    $(`#${word_position}.character`).append(`<div class="casilla" id="${k}"><b>${character_matrix[j][k]}</b></div>`);
+                    $(`#${word_position}.character`).append(`<div class="casilla" style="background-color:tomato;" id="${k}"><b>${character_matrix[j][k]}</b></div>`);
                 } else {
                     $(`#${word_position}.character`).append(`<div class="casilla" id="${k}"></div>`);
                 }
             } 
         }
         $(".character").css({ "width": `${character_matrix.length * 25}`, "height": `${character_matrix.length * 25}`, "grid-template-columns": `repeat(${character_matrix.length}, 1fr)`, "grid-template-rows": `repeat(${character_matrix.length}, 1fr)` });
+    }
+
+    function changeWordSize() {
+        
     }
 });
 
