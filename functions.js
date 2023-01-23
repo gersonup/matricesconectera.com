852cb34cae0ef355701c2d3eb0c30bfcca8ce1ef
Gerson Ricardo Navarro Hernandez <gersonup@gmail.com>
2023-01-23 11:58:48 -0500

    setting arrays

--- functions.js
+++ functions.js
@@ -1,9 +1,9 @@
 let chars;
 var url = "utils.js";
+let word = [];
 $.getScript(url, function(){
     $(document).ready(function(){
         chars = characters;
-        
     });
 });
 
@@ -15,36 +15,35 @@ $(document).ready(function(){
 
         let word = $("#word").val();
         word = word.toUpperCase();
-        var word_array = word.split("");
-        var word_size = word_array.length;
-    
-        createWord(word_array);
+        let word_array = word.split("");
+        let wordMatrix = createWord(word_array);
+        printWord(wordMatrix);
     });
     
 
     function createWord(word_array) {
         for (let i = 0; i < word_array.length; i++) {
-            $(".main").append(`<div class="character" id="${i}"></div>`);
-            createCharacter(characters[word_array[i]], i);
+            let char = word_array[i];
+            word.push(characters[""+char+""]);  
         }
-    }
-  
-    function createCharacter(character_matrix, word_position) {
-        for (let j = 0; j < character_matrix.length; j++) {
-            for (let k = 0; k < character_matrix.length; k++) {
-                if (character_matrix[j][k] == 1){
-                    $(`#${word_position}.character`).append(`<div class="casilla" style="background-color:tomato;" id="${k}"><b>${character_matrix[j][k]}</b></div>`);
-                } else {
-                    $(`#${word_position}.character`).append(`<div class="casilla" id="${k}"></div>`);
-                }
-            } 
-        }
-        $(".character").css({ "width": `${character_matrix.length * 25}`, "height": `${character_matrix.length * 25}`, "grid-template-columns": `repeat(${character_matrix.length}, 1fr)`, "grid-template-rows": `repeat(${character_matrix.length}, 1fr)` });
+        return word
     }
 
     function changeWordSize() {
         
     }
+  
+    function printWord(word) {
+        let html = "";
+        for (let i = 0; i < word.length; i ++) {
+            html += '<div class="caracter">';
+            console.log(word[i]);
+            for (let j = 0; j < word[j].length; j ++ ) {
+                
+            }
+        }
+    }
+    
 });
 
   
