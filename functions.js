06e7e61413267f83566fb07d4a179863d2547cc9
Gerson Navarro <gersonup@gmail.com>
2023-01-28 23:30:10 -0500

    creating patterns

--- functions.js
+++ functions.js
@@ -1,61 +1,48 @@
-let chars;
-var url = "utils.js";
-let word = [];
+var patterns = new Object();
+var matrixChars;
+var size = 5;
+var word = "ABCYR";
+
+
+
+// Llamar el objeto del alfabeto que esta en otro archivo.
+let url = "utils.js";
+
 $.getScript(url, function(){
+    patterns = characters;
+
     $(document).ready(function(){
-        chars = characters;
-    });
-});
-
-$(document).ready(function(){
-    
-    $("#word").change(function(e) {
-        e.preventDefault();
-        $(".main").empty();
-
-        let word = $("#word").val();
-        word = word.toUpperCase();
-        let word_array = word.split("");
-        let wordMatrix = createWord(word_array);
-        printWord(wordMatrix);
-    });
-    
-
-    function createWord(word_array) {
-        for (let i = 0; i < word_array.length; i++) {
-            let char = word_array[i];
-            word.push(characters[""+char+""]);  
-        }
-        return word
-    }
-
-    function changeWordSize() {
-        
-    }
-  
-    function printWord(word) {
-        $("main").empty();
-        let html = "";
-        for (let i = 0; i < word.length; i ++) {
-            html += '<div class="caracter">';
-            console.log(word[i]);
-            for (let j = 0; j < word[i].length; j ++ ) {
-                html += '<div class="casilla"></div>';
-                for ( let k = 0; k < word[i][j]; k++) {
-                    if (word[i][j] == 0) {
-                        html += '<div class="casilla"></div>';
-                    } else {
-                        html += '<div class="casilla">'+word[i][j]+'</div>';
-                    }
+        function fillMatrix(size) {
+            matrixChars = new Array();
+            for (i=0; i<size; i++) {
+                matrixChars[i] = new Array();
+                for (j=0; j<size; j++) {
+                    matrixChars[i][j] = 0;
                 }
-                html += '</div>';   
             }
-            html += '</div>';
         }
-
-        $(".main").append(html);
-    }
-    
+        function writeWord(word) {
+            let charMatrix = new Array();
+            let wordArray = new Array();
+            wordArray = word.split("");
+            for (i=0;i < wordArray.length; i++) {
+                charMatrix[i] = getCharPatterns(wordArray[i]);
+            }
+        }
+        
+        fillMatrix(size);
+        console.log(matrixChars);
+        
+        function getCharPatterns(char) {
+           let pattern = patterns[char];
+           console.log(char,pattern);
+        
+        
+        }
+        
+        function diagonal(){}
+        
+        writeWord(word);
+    });
 });
-
-  
+    
