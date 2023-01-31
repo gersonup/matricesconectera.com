527223b02b7f425a4d00b298c4d1ff541da88dcd
Gerson Navarro <gersonup@gmail.com>
2023-01-30 22:49:53 -0500

    Create matrix chars

--- functions.js
+++ functions.js
@@ -1,48 +1,194 @@
 var patterns = new Object();
-var matrixChars;
 var size = 5;
 var word = "ABCYR";
-
-
 
 // Llamar el objeto del alfabeto que esta en otro archivo.
 let url = "utils.js";
 
 $.getScript(url, function(){
     patterns = characters;
-
     $(document).ready(function(){
+
+        /* Inicializa la matriz */
         function fillMatrix(size) {
-            matrixChars = new Array();
-            for (i=0; i<size; i++) {
+            let matrixChars = new Array();
+            for (i=0; i < size; i++) {
                 matrixChars[i] = new Array();
                 for (j=0; j<size; j++) {
                     matrixChars[i][j] = 0;
                 }
             }
+            return matrixChars;
         }
+
+        /* Obtiene la palabra y la convierte en  */
         function writeWord(word) {
-            let charMatrix = new Array();
-            let wordArray = new Array();
-            wordArray = word.split("");
-            for (i=0;i < wordArray.length; i++) {
-                charMatrix[i] = getCharPatterns(wordArray[i]);
+            
+            let wordArray = word.split("");
+            let wordMatrix = new Array();
+
+            for (i = 0; i < wordArray.length; i++) {
+                let patternsChar = patterns[wordArray[i]];
+                let charMatrix = fillMatrix(size);
+
+                for (const key in patternsChar) {
+                    const axis = key.includes("x")?"x":"y";
+                    const position = key.includes("0") ? 0 : (key.includes("50")? 50: 100);
+                    charMatrix = resolvePattern(patternsChar[key],size,axis,position, charMatrix);
+                }
+
+                wordMatrix[i] = charMatrix;
             }
+
+            return wordMatrix;
+
         }
         
         fillMatrix(size);
-        console.log(matrixChars);
-        
-        function getCharPatterns(char) {
-           let pattern = patterns[char];
-           console.log(char,pattern);
-        
-        
+        console.log(writeWord(word));
+
+        function resolvePattern(pattern, n, axis, position, matrix) {
+            let patternArray = new Array();
+            let n50 = Math.ceil(n/2);
+            console.log(n50);
+            /* Initial Array */
+            for (let i = 0 ; i < n; i ++) {
+                patternArray[i] = 0;
+            }
+            
+            switch (pattern) {
+            case '+all':
+                for (let i = 0 ; i < n; i ++) {
+                    patternArray[i] = 1;
+                }
+                break;
+            case '-all':
+                break;
+            case '+all-1':
+                for (let i = 0 ; i < n-1; i ++) {
+                    patternArray[i] = 1;
+                }
+                break;
+            case '-1+all-1+all-1':
+                for (let i = 1; i < n50-1; i++) {
+                    patternArray[i] = 1;         
+                }
+                for (let i = n50; i < n-1; i++) {
+                    patternArray[i] = 1;         
+                }
+                break;
+            case '-1+all-1':
+                for (let i = 1 ; i < n-1; i ++) {
+                    patternArray[i] = 1;
+                }
+                break;
+            case '-1+1-all+1-1':
+                patternArray[n-2] = 1;
+                patternArray[1] = 1;
+                break;
+            case '+1-all+1':
+                patternArray[n-1] = 1;
+                patternArray[0] = 1;
+                break;
+            case '+50%':
+                for (let i = 0 ; i < n50-1; i ++) {
+                    patternArray[i] = 1;
+                }
+                break;
+            case '-all+50%':
+                for (let i = n50-1 ; i < n; i ++) {
+                    patternArray[i] = 1;
+                }
+                break;
+            case '-1+all-50%':
+                for (let i = 1 ; i < n50-1; i ++) {
+                    patternArray[i] = 1;
+                }
+                break;
+            }
+
+            if (pattern.includes("x")){
+                let counter = 0;
+
+
+
+                let xyPatterns = pattern.split(",");
+
+                for (let k = 0 ; k < xyPatterns.length; k++) {
+                    switch(xyPatterns[k]) {
+                        case '(+x+y)':
+                            counter = n50-1;
+                            for (let i = n50-1; i < n ; i ++ ) {
+                                matrix[counter][i] = 1;
+                                counter --;
+                            }  
+                            break;
+                        case '(+x-y)':
+                            for (let i = n50-1; i < n ; i ++ ) {
+                                matrix[i][i] = 1;
+                            }  
+                            break;
+                        case '(-x+y)':
+                            for (let i = 0; i < n50 ; i ++ ) {
+                                matrix[i][i] = 1;
+                            }
+                            break;
+                        case '(-x-y)':
+                            counter = n50-1;
+                            for (let i = n50-1; i < n ; i ++ ) {
+                                matrix[i][counter] = 1;
+                                counter --;
+                            } 
+                            break;
+                    }
+                }
+
+                return matrix;
+            }
+            
+
+            return fillCharMatrix(patternArray,axis, position, matrix)
         }
-        
-        function diagonal(){}
-        
-        writeWord(word);
+
+        function fillCharMatrix(patternResolved, axis, position, matrix) {
+            let n50 = Math.ceil((matrix.length/2));
+            let n = matrix.length-1;
+
+            if (axis == "y") {
+                for(let i = 0; i < patternResolved.length; i ++) {
+
+                    for(let j = 0; j < matrix.length; j ++) {
+                       
+                        if (position == 0) {
+                            matrix[j][0] = patternResolved[i];
+                        } else if (position == 50) {
+                            matrix[j][n50] = patternResolved[i];
+                        } else if (position == 100) {
+                            matrix[j][n] = patternResolved[i];
+                        }
+                        
+                    }
+                }
+            } else if (axis == "x") {
+                for(let i = 0; i < patternResolved.length; i ++) {
+
+                    for(let j = 0; j < matrix.length; j ++) {
+                       
+                        if (position == 0) {
+                            matrix[0][j] = patternResolved[i];
+                        } else if (position == 50) {
+                            matrix[n50][j] = patternResolved[i];
+                        } else if (position == 100) {
+                            matrix[n][j] = patternResolved[i];
+                        }
+                        
+                    }
+                }
+            }
+
+            return matrix;
+        }
+
     });
 });
     
